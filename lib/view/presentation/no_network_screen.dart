9041ce542a8d87a01e6a1943c423b0965811595b
Abolaji Ayeni <ayenibolaji07@gmail.com>
2023-08-18 22:59:26 +0100

    Revert "added no network screen"
    This reverts commit 75f813fb0fdc7e5a5fc9db7689545074e20762bb.

--- lib/view/presentation/no_network_screen.dart
+++ lib/view/presentation/no_network_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shopping_app/data/helpers/style_helper.dart';
 
 class NoNetworkScreen extends ConsumerWidget {
   const NoNetworkScreen({super.key});
@@ -8,17 +8,10 @@ class NoNetworkScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xffF67952),
         title: const Text("No Network"),
       ),
       body: Center(
-        child: Container(
-            width: 128.w,
-            height: 128.h,
-            decoration: BoxDecoration(
-                color: const Color(0xffF67952),
-                borderRadius: BorderRadius.circular(100.r)),
-            child: Image.asset("assets/images/no_network.png")),
+        child: Text("No Network", style: Styles.largeText()),
       ),
     );
   }
